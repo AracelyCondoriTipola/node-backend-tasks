b48440375f393298f718d84b732880a1dbcc0d95
AracelyCondoriTipola <aracely96.ac>
2021-08-22 04:21:13 -0400

    Implememntando Delete

--- index.js
+++ index.js
@@ -2,38 +2,48 @@
 
 var express = require("express");
 var bodyParser = require('body-parser')
-
 var jsonParser = bodyParser.json()
 //var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 var app = express();
-
 var tasks = []
-
 var surrogatekey = 1;
 
 app.get("/", (req, res, next) => {
     res.json("{ 'message': 'Tasks server online'}");
 });
 
+// Crear una nueva tarea
 app.post("/tasks", jsonParser, (req, res, next) => {
     req.body.id = surrogatekey++;
     tasks.push(req.body);
     res.send("OK");
 });
 
+//listar
 app.get("/tasks", (req, res, next) => {
     res.json(tasks);
 });
+
 
 app.listen(3000, () => {
     console.log("Servidor HTTP funcionando");
 });
 
 
-// buscar
-app.get("/task/:taskId", function(req, res){
-    res.send("taskID is set to" + req.params.taskId);
+// bucar
+app.get("/tasks/:taskId", (req, res, next) => {
+    const id = req.params.taskId;
+    const i = tasks.findIndex(x => x.id == id);
+    res.json(tasks[i]);
 });
 
 
+//borrar
+app.delete("/tasks/:taskId" , jsonParser, (req, res, next) => {
+    const id = req.params.taskId;
+    const i = tasks.findIndex(x => x.id == id );
+    tasks.splice(i,1);
+    res.send("delete " + req.params.taskId);
+    
+});
